0e944019d14667ee44131c45740226162b198da1
lexiaofeng <1411582219@qq.com>
2020-01-10 21:50:24 +0800

    111

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Route::get('/','StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
